981ac9d64ee3410202c0b6d99131e7658bc4b612
LookRain <brantscube@hotmail.com>
2017-05-23 18:00:57 +0800

    refactor

---
+++ src/pages/Admin.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+	<h1>
+		lalalalla
+	</h1>
+</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Admin',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
