206013ac1c68563a82ba826b053cae2409f4b627
LookRain <brantscube@hotmail.com>
2017-07-06 01:18:09 +0800

    add reject patient

--- src/pages/HospitalTasks.vue
+++ src/pages/HospitalTasks.vue
@@ -15,7 +15,7 @@
 					<div>
 						<mu-tabs :value="activeTab" @change="handleTabChange" class="my-tabs" >
 							<mu-tab value="tab1" title="Patients Assigned Today" :style="styleObject"/>
-							<mu-tab value="tab2" title="Patients History" :style="styleObject"/>
+							<mu-tab disabled="disabled" value="tab2" title="Patients History" :style="styleObject"/>
 						</mu-tabs>
 						<div v-if="activeTab === 'tab1'">
 							<br>
@@ -34,13 +34,13 @@
 										<mu-td>{{task.PName}}</mu-td>
 										<mu-td>{{task.PTimeStamp}}</mu-td>
 										<mu-td>
-											<mu-select-field v-model="task.PTier" :labelFocusClass="['label-foucs']">
+											<mu-select-field v-model="task.Tier" :labelFocusClass="['label-foucs']">
 												<mu-menu-item v-for="item, index in tierList" :key="index" :value="index+1" :title="item"/>
 											</mu-select-field>
 										</mu-td>
 										<!-- <mu-td>{{task.PStatus}}</mu-td> -->
 										<mu-raised-button label="Recruit" backgroundColor="green" @click="recruit(task)"/>
-										<mu-raised-button label="Reject" backgroundColor="red" @click="reject"/>
+										<mu-raised-button label="Reject" backgroundColor="red" @click="reject(task, index)"/>
 									</mu-tr>
 
 
@@ -53,25 +53,9 @@
 						<div v-if="activeTab === 'tab2'">
 
 							<mu-tbody>
-
-
-
-										<!-- <mu-tr>
-											<mu-td>Steve Brown</mu-td>
-											<mu-td>Rejected <i class="fa fa-circle fa-lg text-red"></i></mu-td>
-											<mu-td><mu-select-field v-model="game1" :labelFocusClass="['label-foucs']">
-										<mu-menu-item v-for="text,index in list" :key="index" :value="index" :title="text" />
-									</mu-select-field></mu-td>
-											<mu-raised-button label="Recruit" backgroundColor="green"/>
-
-										</mu-tr> -->
 									</mu-tbody>
 								</mu-table>	
-
-
-
-							</div>
-							
+							</div>				
 						</div>
 					</div>
 					<!-- /.box-body-->
@@ -93,6 +77,8 @@
 
 			data () {
 				return {
+					disabled: true,
+					test: '',
 					tasks: [],
 					game1: 0,
 					activeTab: 'tab1',
@@ -107,12 +93,24 @@
 				'full-calendar': require('vue-fullcalendar')
 			},
 			methods: {
-				recruit(input, index) {
+				setStatusRecruit(patient) {
+					patient.PStatus = 1
+					return patient
+				},
+				setStatusReject(patient) {
+					patient.PStatus = 2
+					return patient
+				},
+				recruit(input) {
+					if (!input.Tier) {
+						alert('You must provide a Tier that the patient belongs to!')
+						return;
+					}
 					let patientID = input.PId
 					let patientNRIC = {
-						PatientId: input.NRIC
+						PatientId: input.PId
 					}
-					this.$put('patients/' + patientID, input).then(
+					this.$put('patients/' + patientID, this.setStatusRecruit(input)).then(
 						response => {
 							console.log('Success' + response.data)
 						}).catch(err => {
@@ -130,38 +128,54 @@
 							
 							this.tasks.splice(0,1)
 						},
-						reject() {
-							console.log('clicked reject')
-						},
-						handleTabChange (val) {
-							this.activeTab = val
-						}
-					},
-					computed: {
-						username() {
-							return this.$store.state.user.Email
-						}
-					},
-					watch: {
-						username(val) {
-							if (val) {
-								this.$get('assignedpatients/values?username=' + val).then(
-									response => {
-										this.tasks = response.data
-									})
+						reject(input, index) {
+							let patientID = input.PId
+							let patientNRIC = {
+								PatientId: input.PId
 							}
+							this.$put('patients/' + patientID, this.setStatusReject(input)).then(
+								response => {
+									console.log('Patient Rejected' + response.data)
+								}).catch(err => {
+									console.log(err.response.data.Message)
+								})
+
+							console.log('clicked reject')
+							this.tasks.splice(index, 1)
+
+							},
+							handleTabChange (val) {
+								this.activeTab = val
+							}
+						},
+						computed: {
+							username() {
+								return this.$store.state.user.Email
+							}
+						},
+						watch: {
+							username(val) {
+								if (val) {
+									this.$get('assignedpatients/values?username=' + val).then(
+										response => {
+											this.tasks = response.data
+										})
+								}
+							}
+						},
+						created() {
+							this.$get('assignedpatients/values?username=' + this.$store.state.user.Email).then(
+								response => {
+									this.tasks = response.data
+								})
 						}
-					},
-					created() {
+					}
+				</script>
+
+				<style lang="css" scoped>
+					.my-tabs {
+
+						background-color: #ffffff;
 
 					}
-				}
-			</script>
-
-			<style lang="css" scoped>
-				.my-tabs {
-
-					background-color: #ffffff;
-
-				}
-			</style>
+				</style>
