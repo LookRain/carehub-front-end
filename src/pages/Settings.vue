d9e63b348ddee460130798178d5a87b3fa817619
LookRain <brantscube@hotmail.com>
2017-07-23 13:01:50 +0800

    add register for new user

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -2,8 +2,20 @@
 	<div>
 		<br>
 		
-
+		
 		<div class="row">
+			<section class="col-lg-12 col-xs-12">
+				<div class="box box-success">
+					<div class="box-header">
+					<h3>Register for new user</h3>
+					</div>
+					<div class="box-body">
+						<a href="/register.html" target="_blank">Click here</a>
+					</div>
+				</div>
+			</section>
+
+
 			<section class="col-lg-12 col-xs-12 connectedSortable">
 				<div class="box box-success">
 					<div class="box-header">
@@ -98,8 +110,8 @@
 					return
 				} else {
 					this.$post('wardassignment', {WardNo: this.newWardNo, UserName: this.newStaffEmail}).then(response=> {
-					alert('Added successfully! ' + 'User ' + response.data.UserName + ' assigned to ward ' + response.data.WardNo)
-				})
+						alert('Added successfully! ' + 'User ' + response.data.UserName + ' assigned to ward ' + response.data.WardNo)
+					})
 				}
 				this.close()
 			}
