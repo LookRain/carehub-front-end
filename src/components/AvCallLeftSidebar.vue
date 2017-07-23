4e942e058bafe26e338c613833a48fe4984d6a5b
LookRain <brantscube@hotmail.com>
2017-07-23 18:03:38 +0800

    clean up

--- src/components/AvCallLeftSidebar.vue
+++ src/components/AvCallLeftSidebar.vue
@@ -5,16 +5,7 @@
       <!-- Sidebar user panel -->
       <av-sidebar-header></av-sidebar-header>
       
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
+
       <ul class="sidebar-menu">
         <li class="header">MAIN NAVIGATION</li>
        
